e271b3c2b4c3527a2eb8a43c1454ad528db50bb2
kfang2 <47185626+kfang2@users.noreply.github.com>
2019-02-22 18:38:05 -0500

    ia

---
+++ akshay.php
@@ -0,0 +1,19 @@
+<?php include "inc/html-top.inc";?>
+<main>
+	<a href="http://www.csc174.org/assignment02/hell/akshay.php">
+	<h2>Akshay Desai</h2>
+		<img src="images/akshay.jpg" alt="me" class="elon">
+	</a>
+
+<h3> Hometown</h3>
+	 <p>I am originally from Chicago, IL and moved to Philadelphia, PA during my freshman year of high school. There, I took my first computer science class that piqued my interest in the subject. This interest however developed late in high school, so I started school at the University of Rochester actually as an international relations major. </p>
+<h3> Introduction</h3>
+	<p>I concurrently took a computer science course and significantly enjoyed it more, so I changed major to computer science and haven’t looked back.I’ve worked a few jobs relating to computer science. I founded a startup called Change, there, I was CTO and Lead Software Engineer leading 3 other developers. The purpose of the startup was to connect donors and volunteers to student groups and charities in the Rochester area. We received first-round funding from the National Science Foundation and were a featured startup in the NextCorps Incubator. I also had an internship last summer as a Software Development Engineer at Remitly, an international money transfer company based in Seattle. I worked as a full-stack developer in the Global Product Team, working to ease customer use and better their experiences. My project, specifically, was to create a service for receivers of the money transfer to live track the money. This project involved building the front end for the user to see, the database interaction to get the data, and everything in between. I will be returning to the same team as a full-time engineer upon graduation.</p>
+<h3> School Life</h3>
+	<p>I took many interesting courses in computer science. I started from the beginning, taking “The Science of Programming”, The University of Rochester’s introduction class to computer science. Since then, I’ve taken many classes and even took more classes than were required for the major. A few notable ones are “Operating Systems”, Advanced Algorithms”, “Web Development”, and “Android Development”; in these classes, I’ve learned many concepts and techniques important to being a software engineer. I also have been a Workshop Leader/ Teaching Assistant beginning my second semester of college. I led workshops for “The Science of Programming” and then “Data Structures and Algorithms”. Later in my academic career, I became a Teaching Assistant for “Computer Organization”. Along with those leader experiences, I also am a Resident Advisor (RA), where I live with my residents and ensure a happy living environment which is both safe and enjoyable. In my spare time, other than coding, I dance on a competitive dance team called Rochester Bhangra. It is an Indian dance from the state of Punjab and we travel to competitions every year; Toronto, Chicago, DC, and NYC to name a few.</p>
+	<footer>
+	©  CSC 174 - Advanced Front-end Web Design and Development
+	</footer>
+</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php $style = 'css/home.css'; ?>
+<?php include "inc/html-top.inc";?>
+<?php include "inc/nav.inc";?>
+
+
+<main>
+	<div class="slider">
+			<img class="flexible" src="images/philly.jpg" alt="Philedelphia">
+			<img class="flexible" src="images/ny.jpg" alt="New Yorks">
+			<img class="flexible" src="images/ca.jpg" alt="California">
+	</div>
+
+<div>
+
+<h2>
+	East Coast: Marshia&Akashay
+</h2>
+
+<ul>
+			<li>Seniors at the University of Rochester</li>
+			<li>Majoring in Computer Science</li>
+			<li>Incoming Software Engineer</li>
+		</ul>
+
+
+</div>
+<div>
+
+<h2>
+	West Coast: Genessis	
+</h2>
+<ul>
+			<li>Major in Business</li>
+			<li>Sophomore in UofR</li>
+		</ul>
+
+</div>
+
+<footer>
+	©  CSC 174 - Advanced Front-end Web Design and Development
+	</footer>
+</main>
+
+
+
+
+</body>
+</html>
