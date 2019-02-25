0b67aaf4359edd3fdb60745add390181059b6217
qfu4 <qfu4@u.rochester.edu>
2019-02-24 21:01:43 -0500

    changed h3

--- maisha.php
+++ maisha.php
@@ -6,13 +6,13 @@
 		<h2>Maisha Idris</h2>
 		<img src="images/maisha.jpg" alt="Picture of me">
 	</a>
-	<h3> Hometown</h3>
+	<h3>Hometown</h3>
 	<p>I’m the first in my family to graduate high school (and subsequently the first in my family to graduate college), so it’s a weird shock to be earning a degree in a field I’ve felt (and still sometimes feel) inadequate in. It's no secret that there are fewer women than men in computing, though I've learned of and found a community for myself. It’s scary to imagine becoming a full-fledged adult. Being at UR and away from my home in Queens, NY has definitely helped me gain the necessary knowledge and independence. I’m ready, but hesitant, for what’s ahead.</p>
 
-	<h3> Introduction</h3>
+	<h3>Academic & Career</h3>
 	<p>As long as I don’t fail any classes, I should be graduating this upcoming May. After that, I’m starting a software engineering position right outside of Boston. It’s funny that, somehow, I’ve made it through four years of college, stuck with my Computer Science major, and am now employed in a position that’ll apply my Computer Science knowledge! Along with my major, I am minoring in Political Science and have completed my cluster in American Sign Language. If time allowed it, I would be interested in minoring in ASL as well. I have two Deaf relatives, and I wish I was better able to communicate with them, so I'm glad I was able to learn more about Deaf culture through my cluster. </p>
 
-	<h3> School Life</h3>
+	<h3>Extracurricular Activities</h3>
 	<p>I work part time as a Technical Assistant at the University of Rochester School of Nursing, where I provide support in IT, Facilities, and Educational Media. Workshop leading/TAing was also a source of income in the past, though I’ve let that go for my last semester of college. Additionally, I am a Kearns Scholar and a Hajim Scholar, two academic distinctions I’m proud of. Extracurricular-wise, I recently finished my term as the Public Relations Chair of WiC (Women in Computing), a club I was (and still am!) extremely dedicated to. I’ve been fortunate to do so much in WiC, including alumni video interviews in conjunction with the Office of Admissions, merchandise production, a successful #SupportWomenInCS campaign, and a burgeoning PhD-Undergraduate Computer Science program! Along with my work in WiC, I am a co-founder of 1st Gen Society, the home for the first-generation college students at UR.</p>
 
 	<footer>
